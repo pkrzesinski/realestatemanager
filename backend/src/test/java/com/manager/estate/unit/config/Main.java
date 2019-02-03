4ae49c367a729a01257704567d1a2c68d08a32e0
Pawel Krzesinski <krzesinski.pawel@gmail.com>
2019-02-03 18:30:17 +0100

    Constants of certain methods added.

--- backend/src/test/java/com/manager/estate/unit/config/Main.java
+++ backend/src/test/java/com/manager/estate/unit/config/Main.java
@@ -14,6 +14,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    public static final List<Property> PROPERTIES = getProperties();
+    public static final List<Apartment> APARTMENTS = getApartmentsForProperty();
+    public static final List<Meter> METERS = getMetersForApartment();
 
     public static List<Property> getProperties() {
         List<Property> properties = new ArrayList<>();
@@ -25,10 +28,10 @@ public class Main {
     public static List<Apartment> getApartmentsForProperty() {
         List<Apartment> apartments = new ArrayList<>();
 
-        Apartment apartment1 = new Apartment(1L, "1", -1, 2, 38.5, "Basement", getProperties().get(0));
-        Apartment apartment2 = new Apartment(2L, "2", 0, 1, 22.5, "Ground Floor", getProperties().get(0));
-        Apartment apartment3 = new Apartment(3L, "3", 1, 3, 65.0, "1st floor", getProperties().get(0));
-        Apartment apartment4 = new Apartment(4L, "4", 2, 5, 150.3, "2nd floor", getProperties().get(0));
+        Apartment apartment1 = new Apartment(1L, "1", -1, 2, 38.5, "Basement", PROPERTIES.get(0));
+        Apartment apartment2 = new Apartment(2L, "2", 0, 1, 22.5, "Ground Floor", PROPERTIES.get(0));
+        Apartment apartment3 = new Apartment(3L, "3", 1, 3, 65.0, "1st floor", PROPERTIES.get(0));
+        Apartment apartment4 = new Apartment(4L, "4", 2, 5, 150.3, "2nd floor", PROPERTIES.get(0));
         apartments.addAll(Arrays.asList(apartment1, apartment2, apartment3, apartment4));
         return apartments;
     }
@@ -37,13 +40,13 @@ public class Main {
         List<Tenant> tenants = new ArrayList<>();
 
         Tenant tenant1 = new Tenant(1L, "Adam", "Kowalski", "adam@kowalski.pl", "500600700",
-                "haslo123", "AD 456789", LocalDate.of(1992, 6, 1), getApartmentsForProperty().get(0));
+                "haslo123", "AD 456789", LocalDate.of(1992, 6, 1), APARTMENTS.get(0));
         Tenant tenant2 = new Tenant(2L, "Anna", "Nowak", "Anna@Nowak.pl", "+48 502 756 723",
-                "Anna123", "AD098293", LocalDate.of(1965, 4, 23), getApartmentsForProperty().get(1));
+                "Anna123", "AD098293", LocalDate.of(1965, 4, 23), APARTMENTS.get(1));
         Tenant tenant3 = new Tenant(3L, "Karol", "Smith", "Smith.karol@wp.pl", "665743934",
-                "123456", "YH 765783", LocalDate.of(1994, 1, 1), getApartmentsForProperty().get(2));
+                "123456", "YH 765783", LocalDate.of(1994, 1, 1), APARTMENTS.get(2));
         Tenant tenant4 = new Tenant(4L, "Paweł", "Wójcik", "Wojcik@gmail.com", "50208214",
-                "Password123", "AG 0929454", LocalDate.of(1992, 5, 29), getApartmentsForProperty().get(3));
+                "Password123", "AG 0929454", LocalDate.of(1992, 5, 29), APARTMENTS.get(3));
         tenants.addAll(Arrays.asList(tenant1, tenant2, tenant3, tenant4));
         return tenants;
     }
@@ -51,18 +54,18 @@ public class Main {
     public static List<Meter> getMetersForApartment() {
         List<Meter> meters = new ArrayList<>();
 
-        Meter water1 = new Meter(1L, "12345", MediaType.WATER, getApartmentsForProperty().get(0));
-        Meter gas1 = new Meter(2L, "456", MediaType.GAS, getApartmentsForProperty().get(0));
-        Meter electricity1 = new Meter(3L, "8753", MediaType.ELECTRICITY, getApartmentsForProperty().get(0));
-        Meter water2 = new Meter(4L, "abcde", MediaType.WATER, getApartmentsForProperty().get(1));
-        Meter gas2 = new Meter(5L, "kjlfk", MediaType.GAS, getApartmentsForProperty().get(1));
-        Meter electricity2 = new Meter(6L, "876yhg", MediaType.ELECTRICITY, getApartmentsForProperty().get(1));
-        Meter water3 = new Meter(7L, "73urh", MediaType.WATER, getApartmentsForProperty().get(2));
-        Meter gas3 = new Meter(8L, "ubgh34", MediaType.GAS, getApartmentsForProperty().get(2));
-        Meter electricity3 = new Meter(9L, "657wufe", MediaType.ELECTRICITY, getApartmentsForProperty().get(2));
-        Meter water4 = new Meter(10L, "54rf43", MediaType.WATER, getApartmentsForProperty().get(3));
-        Meter gas4 = new Meter(11L, "4324f45", MediaType.GAS, getApartmentsForProperty().get(3));
-        Meter electricity4 = new Meter(12L, "ger234", MediaType.ELECTRICITY, getApartmentsForProperty().get(3));
+        Meter water1 = new Meter(1L, "12345", MediaType.WATER, APARTMENTS.get(0));
+        Meter gas1 = new Meter(2L, "456", MediaType.GAS, APARTMENTS.get(0));
+        Meter electricity1 = new Meter(3L, "8753", MediaType.ELECTRICITY, APARTMENTS.get(0));
+        Meter water2 = new Meter(4L, "abcde", MediaType.WATER, APARTMENTS.get(1));
+        Meter gas2 = new Meter(5L, "kjlfk", MediaType.GAS, APARTMENTS.get(1));
+        Meter electricity2 = new Meter(6L, "876yhg", MediaType.ELECTRICITY, APARTMENTS.get(1));
+        Meter water3 = new Meter(7L, "73urh", MediaType.WATER, APARTMENTS.get(2));
+        Meter gas3 = new Meter(8L, "ubgh34", MediaType.GAS, APARTMENTS.get(2));
+        Meter electricity3 = new Meter(9L, "657wufe", MediaType.ELECTRICITY, APARTMENTS.get(2));
+        Meter water4 = new Meter(10L, "54rf43", MediaType.WATER, APARTMENTS.get(3));
+        Meter gas4 = new Meter(11L, "4324f45", MediaType.GAS, APARTMENTS.get(3));
+        Meter electricity4 = new Meter(12L, "ger234", MediaType.ELECTRICITY, APARTMENTS.get(3));
         meters.addAll(Arrays.asList(water1, gas1, electricity1, water2, gas2, electricity2, water3, gas3, electricity3,
                 water4, gas4, electricity4));
         return meters;
@@ -70,30 +73,30 @@ public class Main {
 
     public static List<MeterValue> getMeterValuesForJanuary() {
         List<MeterValue> meterValuesJanuary = new ArrayList<>();
-        MeterValue readingWater1Ap1 = new MeterValue(1L, getMetersForApartment().get(0), 34.2);
-        MeterValue readingWater1Ap2 = new MeterValue(4L, getMetersForApartment().get(3), 100.3);
-        MeterValue readingWater1Ap3 = new MeterValue(7L, getMetersForApartment().get(6), 324.2);
-        MeterValue readingWater1Ap4 = new MeterValue(10L, getMetersForApartment().get(9), 65.3);
+        MeterValue readingWater1Ap1 = new MeterValue(1L, METERS.get(0), 34.2);
+        MeterValue readingWater1Ap2 = new MeterValue(4L, METERS.get(3), 100.3);
+        MeterValue readingWater1Ap3 = new MeterValue(7L, METERS.get(6), 324.2);
+        MeterValue readingWater1Ap4 = new MeterValue(10L, METERS.get(9), 65.3);
         meterValuesJanuary.addAll(Arrays.asList(readingWater1Ap1, readingWater1Ap2, readingWater1Ap3, readingWater1Ap4));
         return meterValuesJanuary;
     }
 
     public static List<MeterValue> getMeterValuesForMarch() {
         List<MeterValue> meterValuesMarch = new ArrayList<>();
-        MeterValue readingWater2Ap1 = new MeterValue(2L, getMetersForApartment().get(0), 43.5);
-        MeterValue readingWater2Ap2 = new MeterValue(3L, getMetersForApartment().get(3), 150.2);
-        MeterValue readingWater2Ap3 = new MeterValue(8L, getMetersForApartment().get(6), 453.5);
-        MeterValue readingWater2Ap4 = new MeterValue(11L, getMetersForApartment().get(9), 74.4);
+        MeterValue readingWater2Ap1 = new MeterValue(2L, METERS.get(0), 43.5);
+        MeterValue readingWater2Ap2 = new MeterValue(3L, METERS.get(3), 150.2);
+        MeterValue readingWater2Ap3 = new MeterValue(8L, METERS.get(6), 453.5);
+        MeterValue readingWater2Ap4 = new MeterValue(11L, METERS.get(9), 74.4);
         meterValuesMarch.addAll(Arrays.asList(readingWater2Ap1, readingWater2Ap2, readingWater2Ap3, readingWater2Ap4));
         return meterValuesMarch;
     }
 
     public static List<MeterValue> getMeterValuesForJune() {
         List<MeterValue> meterValuesJune = new ArrayList<>();
-        MeterValue readingWater3Ap1 = new MeterValue(3L, getMetersForApartment().get(0), 56.3);
-        MeterValue readingWater3Ap2 = new MeterValue(6L, getMetersForApartment().get(3), 204.9);
-        MeterValue readingWater3Ap3 = new MeterValue(9L, getMetersForApartment().get(6), 566.3);
-        MeterValue readingWater3Ap4 = new MeterValue(12L, getMetersForApartment().get(9), 100.9);
+        MeterValue readingWater3Ap1 = new MeterValue(3L, METERS.get(0), 56.3);
+        MeterValue readingWater3Ap2 = new MeterValue(6L, METERS.get(3), 204.9);
+        MeterValue readingWater3Ap3 = new MeterValue(9L, METERS.get(6), 566.3);
+        MeterValue readingWater3Ap4 = new MeterValue(12L, METERS.get(9), 100.9);
         meterValuesJune.addAll(Arrays.asList(readingWater3Ap1, readingWater3Ap2, readingWater3Ap3, readingWater3Ap4));
         return meterValuesJune;
     }
